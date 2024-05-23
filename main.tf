4feb7b188f8dbcee44e77ed787892b87568ce73d
TichaTin <ticha@arlula.com>
2024-05-23 17:31:36 +1000

    update bucket access config make public

--- main.tf
+++ main.tf
@@ -4,3 +4,34 @@ resource "aws_s3_bucket" "tichaterraformpracticebucket" {
   bucket = var.bucketname
 }
 # then run terraform plan to review what has changed
+
+
+# define the ownership of the bucket
+resource "aws_s3_bucket_ownership_controls" "bucketOwner" {
+  bucket = aws_s3_bucket.tichaterraformpracticebucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+# make bucket public
+resource "aws_s3_bucket_public_access_block" "bucketPublic" {
+  bucket = aws_s3_bucket.tichaterraformpracticebucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# access control configuration
+resource "aws_s3_bucket_acl" "accessControl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.bucketOwner,
+    aws_s3_bucket_public_access_block.bucketPublic,
+  ]
+
+  bucket = aws_s3_bucket.tichaterraformpracticebucket.id
+  acl    = "public-read"
+}
